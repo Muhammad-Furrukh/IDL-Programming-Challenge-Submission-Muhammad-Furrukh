6b3166319be0ee7d51f35b8a917a9115cc3d1484
Muhammad-Furrukh <mfurrukh12082002@gmail.com>
2025-02-08 09:13:41 +0500

    Comments need adjustment

--- PMP_check.c
+++ PMP_check.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+#include <math.h>
 
 void PMP_Check(char *filepath, char *address, char mode, char oper);
 void Bin_convert(char pmp_cnfgn[][9], int num_reg);
 void mode_store(char pmp_cnfgn[64][9], int addr_mode[64]);
-int MODE_check(char pmp_cnfgn_reg[9]);
+int MODE_check(char pmp_cnfgn[64][9], int pointer);
 int addr_search(char pmp_addr[64][13], int addr_mode[64], char *addr);
 int TOR_addr_check(char pmp_addr[64][13], char *addr, int pointer);
+int NA4_addr_check(char pmp_addr_reg[13], char *addr);
+int NAPOT_addr_check(char *pmp_addr_reg, char *addr);
+void addr_bin_conv(char *pmp_addr_reg, char *bin_addr);
+uint64_t NAPOT_offset(char bin_addr[35]);
+uint64_t NAPOT_base(char bin_addr[35]);
+int checkLock(char pmp_cnfgn[64][9]);
 void Perm_check(char pmp_cnfgn[][9], char oper, int pointer);
-int addr_search_NA4(char pmp_addr[][13], int num_reg, char *addr, int pointer);
 
 char *bin[] = {"0000", "0001", "0010", "0011", "0100", "0101", "0110", "0111", 
                "1000", "1001", "1010", "1011", "1100", "1101", "1110", "1111"};
@@ -97,29 +104,21 @@ void PMP_Check(char *filepath, char *address, char mode, char oper){
     int addr_mode[64];
     mode_store(pmpcnfgn, addr_mode);
     int x = addr_search(pmpaddr, addr_mode, address); // Searches for the relevant pmp address and config register number
-    printf("%s\n", pmpcnfgn[x]);
-    // Checking address matching mode of the found register
-    int addr_mode = MODE_check(pmpcnfgn, 64, x);
-    printf("%d\n", addr_mode);
-
-    if (addr_mode == 1){
-        // TOR Mode confirmed
-    }
-    else { 
-        // Assuming NA4 mode
-        x = x - 1;
-        addr_mode = MODE_check(pmpcnfgn, 64, x);
-        if (addr_mode == 2){
-        // NA4 Mode confirmed
-        }
-        else{
-            // Assuming NAPOT mode
-        }
-    }
     
     // Correct configuration register has been found 
     // Checking permissions for the memory block
-    if ((pmpcnfgn[x][0] == '0') && mode == 'M'){
+    if ((x == 100) && ((mode == 'U') || (mode == 'S'))){
+        printf("Access fault.\n");
+    }
+    else if ((x == 100) && (mode = 'M')){
+        if (checkLock(pmpcnfgn)){
+            printf("Access fault.\n");
+        }
+        else{
+            printf("No access fault.\n");
+        }
+    }
+    else if ((pmpcnfgn[x][0] == '0') && mode == 'M'){
         printf("No access fault.\n");
     }
     else{
@@ -177,11 +176,11 @@ void Bin_convert(char pmp_cnfgn[][9], int num_reg){
 void mode_store(char pmp_cnfgn[64][9], int addr_mode[64]){
     int i;
     for (i = 0; i < 64; i++){
-        addr_mode[i] = MODE_check(pmp_cnfgn[i]);
+        addr_mode[i] = MODE_check(pmp_cnfgn, i);
     }
 }
 
-int MODE_check(char pmp_cnfgn_reg[9]){
+int MODE_check(char pmp_cnfgn[64][9], int pointer){
     /* Function to check address mode of a given configuration register
     Takes 3 arguments:
     1. pmp_cnfgn[][9]: A 2D array containing the data of all the pmp configuration registers
@@ -189,13 +188,13 @@ int MODE_check(char pmp_cnfgn_reg[9]){
     3. pointer: register number for which the address mode is to be checked 
     Returns an int corresponding to address modes of the pmp configuration register
     */
-    if ((pmp_cnfgn_reg[3] == '0') && (pmp_cnfgn_reg[4] == '0')){
+    if ((pmp_cnfgn[pointer][3] == '0') && (pmp_cnfgn[pointer][4] == '0')){
         return 0;
     }
-    else if ((pmp_cnfgn_reg[3] == '0') && (pmp_cnfgn_reg[4] == '1')){
+    else if ((pmp_cnfgn[pointer][3] == '0') && (pmp_cnfgn[pointer][4] == '1')){
         return 1;
     }
-    else if ((pmp_cnfgn_reg[3] == '1') && (pmp_cnfgn_reg[4] == '0')){
+    else if ((pmp_cnfgn[pointer][3] == '1') && (pmp_cnfgn[pointer][4] == '0')){
         return 2;
     }
     else{
@@ -227,6 +226,7 @@ int addr_search(char pmp_addr[64][13], int addr_mode[64], char *addr){
         }
         i++;
     }
+    return 100;
 }
 
 int TOR_addr_check(char pmp_addr[64][13], char *addr, int pointer){
@@ -256,9 +256,9 @@ int TOR_addr_check(char pmp_addr[64][13], char *addr, int pointer){
         temp1++;
     }
 
-    long pointer1_dec = strtol(pmp_addr[pointer], NULL, 16);
-    long pointer2_dec = strtol(pmp_addr[pointer-1], NULL, 16);
-    long input_dec = strtol(addr, NULL, 16);
+    uint64_t pointer1_dec = strtol(pmp_addr[pointer], NULL, 16);
+    uint64_t pointer2_dec = strtol(pmp_addr[pointer-1], NULL, 16);
+    uint64_t input_dec = strtol(addr, NULL, 16);
 
     if ((input_dec >= pointer2_dec) && (input_dec < pointer1_dec)){
         return 1;
@@ -276,8 +276,8 @@ int NA4_addr_check(char pmp_addr_reg[13], char *addr){
             }
             temp++;
         }
-    long pointer_dec = strtol(pmp_addr_reg, NULL, 16);
-    long input_dec = strtol(addr, NULL, 16);
+    uint64_t pointer_dec = strtol(pmp_addr_reg, NULL, 16);
+    uint64_t input_dec = strtol(addr, NULL, 16);
 
     if ((input_dec >= pointer_dec) && (input_dec < pointer_dec + 3)){
         return 1;
@@ -287,6 +287,99 @@ int NA4_addr_check(char pmp_addr_reg[13], char *addr){
     }
 }
 
+int NAPOT_addr_check(char *pmp_addr_reg, char *addr){
+    char bin_pmp_addr[35];
+    addr_bin_conv(pmp_addr_reg, bin_pmp_addr);
+    uint64_t offset = NAPOT_offset(bin_pmp_addr);
+    uint64_t base = NAPOT_base(bin_pmp_addr);
+
+    uint64_t search_addr = strtol(addr, NULL, 16);
+    if ((search_addr >= base) && (search_addr < base + offset)){
+        return 1;
+    }
+    else{
+        return 0;
+    }
+}
+
+void addr_bin_conv(char *pmp_addr_reg, char *bin_addr){
+    char temp[35]; // temporary variable to store binary string
+    int val;
+    int i = 2;
+    int pos = 0;
+
+    // Inserting missing leading 0's
+    char *end_ptr = strchr(pmp_addr_reg, '\n');
+    int len = end_ptr - pmp_addr_reg - 2;
+    char *two_zero = "00";
+    strncpy(temp + pos, two_zero, 2);
+    pos += 2;
+    for (int j = 0; j < 9 - len - 1; j++){
+        strncpy(temp + pos, bin[0], 4);
+        pos += 4;
+    }
+
+    while (pmp_addr_reg[i] != '\n'){
+        if ((pmp_addr_reg[i] >= '0' ) && (pmp_addr_reg[i] <= '9')){
+            val = pmp_addr_reg[i] - '0';
+            strncpy(temp + pos, bin[val], 4);
+        }
+        else if ((pmp_addr_reg[i] >= 'A') && (pmp_addr_reg[i] <= 'F')){
+            val = pmp_addr_reg[i] - 'A' + 10;
+            strncpy(temp + pos, bin[val], 4);
+        }
+        else{
+            val = pmp_addr_reg[i] - 'a' + 10;
+            strncpy(temp + pos, bin[val], 4);
+        }
+        pos += 4; 
+        i++;
+    }
+    temp[34] = '\0';
+    strcpy(bin_addr, temp);
+}
+
+uint64_t NAPOT_offset(char bin_addr[35]){
+    int i = 31;
+    int power = 2; 
+    while (i > 1){
+        power++;
+        if (bin_addr[i] == '0'){
+
+            break;
+        }
+        i--;
+    }
+    uint64_t offset = 1;
+    for (int j = 0; j < power; j++){
+        offset *= 2;
+    }
+    return offset;
+}
+
+uint64_t NAPOT_base(char bin_addr[35]){
+    int i = 31; 
+    while (i > 1){
+        if (bin_addr[i] == '0'){
+            break;
+        }
+        else{
+            bin_addr[i] = '0';
+        }
+        i--;
+    }
+    uint64_t base = strtol(bin_addr, NULL, 2);
+    return base;
+}
+
+int checkLock(char pmp_cnfgn[64][9]){
+    for (int i = 0; i < 64; i++){
+        if (pmp_cnfgn[i][0] == '1'){
+            return 1;
+        }
+    }
+    return 0;
+}
 
 
 void Perm_check(char pmp_cnfgn[][9], char oper, int pointer){
